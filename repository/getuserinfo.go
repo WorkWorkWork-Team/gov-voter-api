261eab7725db020456fbac65c77042896fc02d41
TaninDean <reiodean.dean@gmail.com>
2022-10-29 21:33:02 +0700

    fix: change query function from Select to Get

--- repository/getuserinfo.go
+++ repository/getuserinfo.go
@@ -1,9 +1,6 @@
 package repository
 
 import (
-	"fmt"
-	"strconv"
-
 	model "github.com/WorkWorkWork-Team/gov-voter-api/models"
 	"github.com/jmoiron/sqlx"
 )
@@ -23,19 +20,10 @@ func NewPopulationRepository(mysql *sqlx.DB) PopulationRepository {
 }
 
 func (g *populationRepository) GetUserInfo(citizenID string) (userInfo model.Population, err error) {
-	var getUserInfoList []model.Population
-	fmt.Println(citizenID)
-	var id, _ = strconv.Atoi(citizenID)
-	err = g.mysql.Select(&getUserInfoList, "SELECT * FROM `Population` WHERE citizenID=?", id)
+	var getUserInfoList model.Population
+	err = g.mysql.Get(&getUserInfoList, "SELECT * FROM `Population` WHERE citizenID=?", citizenID)
 	if err != nil {
 		return userInfo, err
 	}
-
-	userInfoLenght := len(getUserInfoList)
-	if userInfoLenght == 0 {
-		return userInfo, ErrNotFound
-	} else if userInfoLenght > 1 {
-		return userInfo, ErrFoundMoreThanOne
-	}
-	return getUserInfoList[0], nil
+	return getUserInfoList, nil
 }
