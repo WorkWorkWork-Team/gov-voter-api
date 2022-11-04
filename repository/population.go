542cf123c38d5034ae61a974eae563ad10d2674a
TaninDean <reiodean.dean@gmail.com>
2022-11-04 11:07:08 +0700

    fix: wrong changing filename

--- repository/population.go
+++ repository/population.go
@@ -1,47 +1,29 @@
 package repository
 
 import (
-	"errors"
-
 	model "github.com/WorkWorkWork-Team/gov-voter-api/models"
 	"github.com/jmoiron/sqlx"
 )
 
-type applyVoteRepository struct {
+type populationRepository struct {
 	mysql *sqlx.DB
 }
 
-type ApplyVoteRepository interface {
-	ApplyVote(citizenID string) error
-	GetApplyVoteByCitizenID(citizenID string) (model.ApplyVote, error)
+type PopulationRepository interface {
+	GetUserInfo(citizenID string) (model.Population, error)
 }
 
-var ErrFoundMoreThanOne error = errors.New("found more than one row")
-var ErrNotFound error = errors.New("not found")
-
-func NewApplyVoteRepository(mysql *sqlx.DB) ApplyVoteRepository {
-	return &applyVoteRepository{
+func NewPopulationRepository(mysql *sqlx.DB) PopulationRepository {
+	return &populationRepository{
 		mysql: mysql,
 	}
 }
 
-func (a *applyVoteRepository) ApplyVote(citizenID string) error {
-	_, err := a.mysql.Query("INSERT INTO ApplyVote (CitizenID) VALUES (?)", citizenID)
-	return err
-}
-
-func (a *applyVoteRepository) GetApplyVoteByCitizenID(citizenID string) (applyVote model.ApplyVote, err error) {
-	var applyVoteList []model.ApplyVote
-	err = a.mysql.Select(&applyVoteList, "SELECT * FROM `ApplyVote` WHERE citizenID=?", citizenID)
+func (g *populationRepository) GetUserInfo(citizenID string) (userInfo model.Population, err error) {
+	var getUserInfoList model.Population
+	err = g.mysql.Get(&getUserInfoList, "SELECT * FROM `Population` WHERE citizenID=?", citizenID)
 	if err != nil {
-		return applyVote, err
+		return userInfo, err
 	}
-
-	applyVoteLength := len(applyVoteList)
-	if applyVoteLength == 0 {
-		return applyVote, ErrNotFound
-	} else if applyVoteLength > 1 {
-		return applyVote, ErrFoundMoreThanOne
-	}
-	return applyVoteList[0], nil
+	return getUserInfoList, nil
 }
